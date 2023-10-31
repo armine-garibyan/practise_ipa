96ff3349aade6e3f36921498c632bb8f303a8754
armine-garibyan <armine-garibyan@yandex.ru>
2023-10-31 22:48:23 +0100

    Release version 1.1.0: New correct answer field; all correct alternatives displayed; answer check disabled after displayed correct answer.

--- version.py
+++ version.py
@@ -1,1 +1,6 @@
-__version__ = "1.0.0"
+__version__ = "1.1.0"
+
+# CHANGES
+# correct answer is now shown in a separate field
+# if more than one variant is possible, all correct answers are displayed
+# answer check is disabled if the correct answer was displayed previously
