7656d6383012266bd42d3e2d379c807affcdf364
armine-garibyan <armine-garibyan@yandex.ru>
2023-11-13 09:12:37 +0100

    Fixed the mistake in the meta tag of the laout template.

--- version.py
+++ version.py
@@ -1,6 +1,4 @@
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 # CHANGES
-# correct answer is now shown in a separate field
-# if more than one variant is possible, all correct answers are displayed
-# answer check is disabled if the correct answer was displayed previously
+# fixed a mistake in the meta tag in the layout tempalte
